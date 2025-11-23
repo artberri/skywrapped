03e8ef7e6f3b293dfad002bbcaa7b0bb62b345dc
Alberto Varela Sánchez <hello@albertovarela.net>
2025-11-23 22:09:29 +0100

    trying to understand private key error

--- src/lib/server/jwk.ts
+++ src/lib/server/jwk.ts
@@ -1,3 +1,4 @@
+import { ensureError } from "$lib/utils";
 import { jwkPubSchema } from "@atproto/oauth-client-node";
 import { z } from "zod";
 
@@ -6,6 +7,12 @@ const jsonWebKeysSchema = z.array(jwkPubSchema).nonempty();
 export type PublicJwks = z.output<typeof jsonWebKeysSchema>;
 
 export const validJsonWebKeys = (input: string): PublicJwks => {
-  const value = JSON.parse(input);
-  return jsonWebKeysSchema.parse(value);
+  try {
+    const value = JSON.parse(input);
+    return jsonWebKeysSchema.parse(value);
+  } catch (error) {
+    throw new Error(
+      `Error parsing PRIVATE_KEYS: "${input}" - ${ensureError(error).message}`,
+    );
+  }
 };
